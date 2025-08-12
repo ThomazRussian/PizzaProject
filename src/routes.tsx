56efd8d43534b67abe923dff354cac974b602a39
Thomaz Russian <thomazrussian@gmail.com>
2025-08-12 19:57:42 -0300

    New:Charts

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom'
 import { Orders } from './pages/app/orders/orders'
 import { AppLayout } from './pages/_layouts/app'
-import { Dashboard } from './pages/app/dashboard'
+import { Dashboard } from './pages/app/dashboard/dashboard'
 import { SignIn } from './pages/auth/sign-in'
 import { AuthLayout } from './pages/_layouts/auth'
 import { SignUp } from './pages/auth/sign-up'
